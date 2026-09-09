91c18af38f5e4937f414db52af110f757c448221
esmael hamdy <esmealhamdy404@gmail.com>
2026-09-09 14:50:00 +0200

    4RD ANSWER IN AS 2

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -24,20 +24,33 @@
             //Console.WriteLine($"title of book =  {book1.Title} ,num of pages = {book1.pages}");
 
             //#endregion
-           
-            #region 4rd question the first in assignment 1
 
-            Book book1 = new Book();
-            book1.Title = "atomic habbit";
-            book1.pages = 300;
-           //Console.WriteLine($"title of book =  {book1.Title} ,num of pages = {book1.pages}");
-           Console.WriteLine(book1.ToString());
-            Console.WriteLine(book1.Equals(book1));
-            Console.WriteLine(book1.GetHashCode());
-            Console.WriteLine(book1.GetType());
+            // #region 4rd question the first in assignment 1
 
+            // Book book1 = new Book();
+            // book1.Title = "atomic habbit";
+            // book1.pages = 300;
+            ////Console.WriteLine($"title of book =  {book1.Title} ,num of pages = {book1.pages}");
+            //Console.WriteLine(book1.ToString());
+            // Console.WriteLine(book1.Equals(book1));
+            // Console.WriteLine(book1.GetHashCode());
+            // Console.WriteLine(book1.GetType());
+            // #endregion
+            try
+            {
+                int x = 1;
+                int y = 0;
+                int z = x/y;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("can not divide by zero");
+            }
+            finally
+            {
+                Console.WriteLine("DONE");
+            }
 
-            #endregion
 
 
 
