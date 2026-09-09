37ab2083170bee515b13725c7c68ef864729ab5c
IT  M <IT  M@DESKTOP-G63PE4Q>
2026-09-09 11:09:22 +0200

    Add project files.

---
+++ Assignment02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 1st quetion
+            string title = "clean code";
+            string UpperTitle = title.ToUpper();
+            Console.WriteLine($"title after upper = {UpperTitle}");
+            Console.WriteLine($"title before = {title}");
+            #endregion
+
+        }
+    }
+}
