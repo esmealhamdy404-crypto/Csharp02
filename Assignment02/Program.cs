bb0602cfc5d8c73857aed21113823f970c94fb74
esmael hamdy <esmealhamdy404@gmail.com>
2026-09-09 13:15:55 +0200

    2nd question

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -11,6 +11,16 @@
             Console.WriteLine($"title after upper = {UpperTitle}");
             Console.WriteLine($"title before = {title}");
             #endregion
+            #region 2nd question
+            string variable1 = "clean code";
+            string variable2 = "clean code";
+            Console.WriteLine(variable1.Equals(variable2));
+
+            #endregion
+            #region
+            // 3rd question
+            #endregion
+
 
         }
     }
